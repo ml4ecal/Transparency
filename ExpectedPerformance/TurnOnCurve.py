e58fbb87459b1039201ffab4ca5ccddedf1f3795
brusale <brusale11@gmail.com>
2020-08-26 18:36:02 +0200

    Python script for TurnOnCurve

--- ExpectedPerformance/TurnOnCurve.py
+++ ExpectedPerformance/TurnOnCurve.py
@@ -6,11 +6,10 @@
 
 import ROOT
 import numpy as np
-import matplotlib.pyplot as pltt
+import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
 from scipy import special
-import random
 
 
 # In[ ]:
@@ -82,8 +81,6 @@ mean_test_2 = mean_test_2[mean_test_2 != -1]
 metadata = metadata.iloc[:len(mean)][mean != -1]
 
 
-# # Every Fill
-
 # In[ ]:
 
 
@@ -142,43 +139,94 @@ delta_value = (massimo-minimo)/nbin
 nEvents = 1000
 
 
-# # Single fill
-
-# In[ ]:
-
-print("Fill:")
-selected_fill = input()
-
+# # Single Fill
 
 # In[ ]:
 
 
-selected_metadata = metadata[metadata["fill_num"] == int(selected_fill)]
-selected_index = selected_metadata.index.values
+fill = input()
 
 
 # In[ ]:
 
 
-selected_transp = [mean[i] for i in selected_index]
-selected_transp = selected_transp/selected_transp[0]
+selected_metadata = metadata[metadata.fill_num == int(fill)]
+selected_transp = [mean[i] for i in selected_metadata.index.tolist()]
 
 
 # In[ ]:
 
 
-c = ROOT.TCanvas("cc_turn_on0", "", 800, 700)
-hist = ROOT.TH1F("real", " ", nbin, minimo, massimo)
+lumi_in_fill = selected_metadata.lumi_in_fill.to_numpy()
+lumi_inst = selected_metadata.lumi_inst.to_numpy()
+lumi_inst_0 = np.empty(np.size(selected_transp))
+lumi_inst_0.fill(lumi_inst[0])
 
 
 # In[ ]:
 
 
-for ibin in range(0,nbin):
+c = ROOT.TCanvas("cc_turn_on", "", 800, 700)
+hist0 = ROOT.TH1F("real", "", nbin, minimo, massimo)
+
+
+# In[ ]:
+
+
+for ibin in range(0, nbin):
     value = minimo+(ibin+0.2)*delta_value
     for iEvent in range(0, nEvents):
         for i in range(0, np.size(selected_transp)):
             value_smeared = value*selected_transp[i]
+            if value_smeared > threshold:
+                hist0.Fill(value)
+
+
+# In[ ]:
+
+
+hist0.Scale(1./(nEvents*np.size(selected_transp)))
+  
+hist0.SetLineWidth(2)
+hist0.SetLineColor(632)
+  
+hist0.Draw("histo")
+hist0.GetXaxis().SetTitle("Energy [GeV]")
+hist0.GetYaxis().SetTitle("Efficiency")
+
+
+# In[ ]:
+
+
+c.Draw()
+vertical_line = ROOT.TLine(threshold, 0.0, threshold, 1.1)
+vertical_line.Draw()
+#c1.SaveAs("h_turn_on.png")
+
+
+# ## $f_1$
+
+# In[ ]:
+
+
+c0 = ROOT.TCanvas("cc_turn_on", "", 800, 700)
+hist = ROOT.TH1F("f_1", "", nbin, minimo, massimo)
+
+
+# In[ ]:
+
+
+transparency = transp_func([lumi_in_fill*(1e-9), lumi_inst*(1e-9), lumi_inst_0*(1e-9)], *par_1)
+
+
+# In[ ]:
+
+
+for ibin in range(0, nbin):
+    value = minimo+(ibin+0.2)*delta_value
+    for iEvent in range(0, nEvents):
+        for i in range(0, np.size(selected_transp)):
+            value_smeared = value*transparency[i]
             if value_smeared > threshold:
                 hist.Fill(value)
 
@@ -190,192 +238,68 @@ hist.Scale(1./(nEvents*np.size(selected_transp)))
   
 hist.SetLineWidth(2)
 hist.SetLineColor(632)
-#hist.Draw("histo")
-
+  
+hist.Draw("histo")
 hist.GetXaxis().SetTitle("Energy [GeV]")
 hist.GetYaxis().SetTitle("Efficiency")
 
 
-# # Third function
-
 # In[ ]:
 
 
-#c1 = ROOT.TCanvas("cc_turn_on", "", 800, 700)
-hist_f1 = ROOT.TH1F("histo", " ", nbin, minimo, massimo)
-
-
-# In[ ]:
-
-
-lumi_in_fill = (selected_metadata.lumi_in_fill*(1e-9)).to_numpy()
-lumi_inst = (selected_metadata.lumi_inst*(1e-9)).to_numpy()
-y_0 = lumi_inst[0]
-lumi_inst_0 = np.empty(np.size(lumi_inst))
-lumi_inst_0.fill(y_0)
-
-
-# In[ ]:
-
-
-transp_smeared = transp_func([lumi_in_fill, lumi_inst, lumi_inst_0], *par_1)
-
-
-# In[ ]:
-
-
-for ibin in range(0,nbin):
-    value = minimo+(ibin+0.2)*delta_value
-    for iEvent in range(0, nEvents):
-        for i in range(0, np.size(transp_smeared)):
-            value_smeared = value*transp_smeared[i]
-            if value_smeared > threshold :
-                hist_f1.Fill(value)
-
-
-# In[ ]:
-
-
-hist_f1.Scale(1./(nEvents*np.size(transp_smeared)))
-  
-hist_f1.SetLineWidth(2)
-hist_f1.SetLineColor(603)
-
-hist.Draw("histo")
-hist_f1.Draw("histo same")
-
-hist_f1.GetXaxis().SetTitle("Energy [GeV]")
-hist_f1.GetYaxis().SetTitle("Efficiency")
-
-
-# In[ ]:
-
-
-c.Draw()
-
-
-# ## Sigmoid
-
-# In[ ]:
-
-
-#sigmoid = ROOT.TF1("sigmoid", "1/(1+exp(-(x-[0])/[1]))", 0, 60)
-#sigmoid.SetParameters(0, threshold)
-
-
-# In[ ]:
-
-
-#r = hist.Fit("sigmoid", "s")
-#r.Print("V")
-
-
-# In[ ]:
-
-
-#fit_1 = hist.GetFunction("sigmoid")
-#fit_1.GetProb()
-
-
-# In[ ]:
-
-
-#c1.Draw()
-#vertical_line = ROOT.TLine(threshold, 0.0, threshold, 1.1)
-#vertical_line.Draw()
+c0.Draw()
+vertical_line = ROOT.TLine(threshold, 0.0, threshold, 1.1)
+vertical_line.Draw()
 #c1.SaveAs("h_turn_on.png")
 
 
-# ## Arctan
+# ## $f_2$
 
 # In[ ]:
 
 
-#arctan = ROOT.TF1("arctan","[2]+[3]*atan([1]*(x-[0]))")
-#arctan.SetParameters(0, threshold)
-
-
-# In[ ]:
-
-
-#r = hist.Fit("arctan", "s")
-#r.Print("V")
+c1 = ROOT.TCanvas("cc_turn_on_2", "", 800, 700)
+hist2 = ROOT.TH1F("f_2", "", nbin, minimo, massimo)
 
 
 # In[ ]:
 
 
-#fit_1 = hist.GetFunction("arctan")
-#fit_1.GetProb()
-
-
-# ## Erf
-
-# In[ ]:
-
-
-#erf = ROOT.TF1("erf", "[0]*TMath::Erf([2]*(x-[1]))+[0]")
-#erf.SetParameters(1, threshold)
-#erf.SetParameters(0, 0.5)
+transparency = transp_func_2([lumi_in_fill[i]*(1e-9), lumi_inst[i]*(1e-9), lumi_inst_0[i]*(1e-9)], *par_2)
 
 
 # In[ ]:
 
 
-#r = hist.Fit("erf", "s")
-#r.Print("V")
+for ibin in range(0, nbin):
+    value = minimo+(ibin+0.2)*delta_value
+    for iEvent in range(0, nEvents):
+        for i in range(0, np.size(selected_transp)):
+            value_smeared = value*transparency
+            if value_smeared > threshold:
+                hist2.Fill(value)
 
 
 # In[ ]:
 
 
-#fit_1 = hist.GetFunction("erf")
-#fit_1.GetProb()
-
-
-# # Second function
-
-# In[ ]:
-
-
-#c2 = ROOT.TCanvas("cc_turn_on2", "", 800, 700)
-#hist2 = ROOT.TH1F("f_2", " ", nbin, minimo, massimo)
-
-
-# In[ ]:
-
-
-#for ibin in range(0,nbin):
-    #value = minimo+(ibin+0.5)*delta_value
-    #for iEvent in range(0, nEvents):
-        #lumi_in_fill = ROOT.gRandom.Uniform(0.7)
-        #lumi_inst = ROOT.gRandom.Uniform(0.0005)
-        #y_0 = ROOT.gRandom.Uniform(0.0005)
-        #value_smeared = value*(transp_func_2([lumi_in_fill, lumi_inst, y_0], *par_2))
-        #if value_smeared > threshold:
-            #hist2.Fill(value)
-
-
-# In[ ]:
-
-
-#hist2.Scale(1./nEvents)
+hist2.Scale(1./(nEvents*np.size(selected_transp)))
   
-#hist2.SetLineWidth(2)
-#hist2.SetLineColor(632)
+hist2.SetLineWidth(2)
+hist2.SetLineColor(632)
   
-#hist2.Draw("histo")
-#hist2.GetXaxis().SetTitle("Energy [GeV]")
-#hist2.GetYaxis().SetTitle("Efficiency")
+hist2.Draw("histo")
+hist2.GetXaxis().SetTitle("Energy [GeV]")
+hist2.GetYaxis().SetTitle("Efficiency")
 
 
 # In[ ]:
 
 
-#c2.Draw()
-#vertical_line = ROOT.TLine(threshold, 0.0, threshold, 1.1)
-#vertical_line.Draw()
-#c2.SaveAs("h_turn_on2.png")
+c1.Draw()
+vertical_line = ROOT.TLine(threshold, 0.0, threshold, 1.1)
+vertical_line.Draw()
+#c1.SaveAs("h_turn_on.png")
 
 
 # In[ ]:
