dad5c149027e9a110eb3ec1a196ec5130bc2bd45
amassiro <massironi.andrea@gmail.com>
2024-11-08 17:14:21 +0100

    up

--- CMSSWvalidation/merge.cxx
+++ CMSSWvalidation/merge.cxx
@@ -12,6 +12,17 @@ void merge(){
   Float_t         energy_EE[14648];
   
 
+  TFile* fileMerged = new TFile ("merged.root", "RECREATE");
+  TTree* mergedTree = new TTree("mergedTree", "");
+  
+  Float_t         energy_EB_all_prompt;
+  Float_t         energy_EB_hlt;
+  Float_t         energy_EB_model;
+  mergedTree->Branch("energy_EB_all_prompt", &energy_EB_all_prompt, "F");
+  mergedTree->Branch("energy_EB_hlt", &energy_EB_hlt, "F");
+  mergedTree->Branch("energy_EB_model", &energy_EB_model, "F");
+  
+  
   TFile *_file0 = TFile::Open("dumpPROMPTRECO.root");
 
   TFile *_file1 = TFile::Open("dumpAVARSI.root");
@@ -40,18 +51,37 @@ void merge(){
     
     tree0->GetEntry(iEntry);
     
-    if (energy_EB[0] >0.001) {
-      std::cout << "   energy_EB[0] = " << energy_EB[0];
-      tree1->GetEntry(iEntry);
-      std::cout << "   energy_EB[0] = " << energy_EB[0];
-      tree2->GetEntry(iEntry);
-      std::cout << "   energy_EB[0] = " << energy_EB[0];
-      std::cout << std::endl;      
+    std::cout << " iEntry = " << iEntry << " : " << nEntries << std::endl;
+    
+    for (int ixtal=0; ixtal<61200; ixtal++) {
+      if (energy_EB[ixtal] >0.00001) {
+//         std::cout << "   energy_EB[0] = " << energy_EB[0];
+        energy_EB_all_prompt = energy_EB[ixtal];
+        
+        tree1->GetEntry(iEntry);
+//         std::cout << "   energy_EB[0] = " << energy_EB[0];
+        energy_EB_model = energy_EB[ixtal];
+        
+        tree2->GetEntry(iEntry);
+//         std::cout << "   energy_EB[0] = " << energy_EB[0];
+        energy_EB_hlt = energy_EB[ixtal];
+        
+//         std::cout << std::endl;    
+        
+        mergedTree->Fill();
+        
+      }
+      
     }
     
     
     
   }
+  
+  
+  fileMerged->cd();
+  mergedTree->Write();
+  
   
   
 }
